cb4de81fb4972f0ad1c53013ef8aa89d4d8240c8
Mateusz Boczarski <matbocz@outlook.com>
2020-04-23 21:14:16 +0200

    Code format

--- renderer.js
+++ renderer.js
@@ -12,41 +12,39 @@ document.querySelector('#searchButton').addEventListener('click', () => {
 
         $("#messageDiv").empty();
         $("#messageDiv").append("<p'>City field is empty. <br>Enter city name and click the button.</p>");
-    }
-
-    else {
+    } else {
         $("#messageDiv").hide();
         $("#resultDiv").show();
 
-        $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&lang=en&appid=" + api_key, function (data) {
-            var iconCode = data.weather[0].icon;
-            var iconUrl = "http://openweathermap.org/img/wn/" + iconCode + ".png";
+        $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&lang=en&appid=" + api_key, function(data) {
+                var iconCode = data.weather[0].icon;
+                var iconUrl = "http://openweathermap.org/img/wn/" + iconCode + ".png";
 
-            $("#resultH5").empty();
-            $("#resultH5").append("<b>Weather in " + data.name + ", " + data.sys.country + "</b><img class='center' id='iconWeather' src='' alt='icon'></img>");
-            $('#iconWeather').attr('src', iconUrl);
+                $("#resultH5").empty();
+                $("#resultH5").append("<b>Weather in " + data.name + ", " + data.sys.country + "</b><img class='center' id='iconWeather' src='' alt='icon'></img>");
+                $('#iconWeather').attr('src', iconUrl);
 
-            $("#temperatureTd").empty();
-            $("#temperatureTd").append(Math.round(data.main.temp) + " °C");
+                $("#temperatureTd").empty();
+                $("#temperatureTd").append(Math.round(data.main.temp) + " °C");
 
-            $("#windTd").empty();
-            $("#windTd").append(data.wind.speed + " m/s");
+                $("#windTd").empty();
+                $("#windTd").append(data.wind.speed + " m/s");
 
-            $("#pressureTd").empty();
-            $("#pressureTd").append(data.main.pressure + " hpa");
+                $("#pressureTd").empty();
+                $("#pressureTd").append(data.main.pressure + " hpa");
 
-            $("#humidityTd").empty();
-            $("#humidityTd").append(data.main.humidity + " %");
+                $("#humidityTd").empty();
+                $("#humidityTd").append(data.main.humidity + " %");
 
-            $("#sunriseTd").empty();
-            $("#sunriseTd").append(moment.unix(data.sys.sunrise).format('LT'));
+                $("#sunriseTd").empty();
+                $("#sunriseTd").append(moment.unix(data.sys.sunrise).format('LT'));
 
-            $("#sunsetTd").empty();
-            $("#sunsetTd").append(moment.unix(data.sys.sunset).format('LT'));
-        })
-        // .fail(function () {
-        //     $("#resultDiv").empty();
-        //     $("#resultDiv").append("<p>Error</p>");
-        // })
+                $("#sunsetTd").empty();
+                $("#sunsetTd").append(moment.unix(data.sys.sunset).format('LT'));
+            })
+            // .fail(function () {
+            //     $("#resultDiv").empty();
+            //     $("#resultDiv").append("<p>Error</p>");
+            // })
     }
 })
