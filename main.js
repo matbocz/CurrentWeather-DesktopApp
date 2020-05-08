fea3513b36dd9a84f0eb075d0be00cc349da23ae
Mateusz Boczarski <matbocz@outlook.com>
2020-05-08 19:04:20 +0200

    Create app menu

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
+
+const template = [{
+    label: 'Menu',
+    submenu: [{
+            label: 'Go to the GitHub repository',
+            click: async() => {
+                const { shell } = require('electron')
+                await shell.openExternal('https://github.com/matbocz/CurrentWeather-DesktopApp')
+            }
+        },
+
+        {
+            label: 'Fullscreen',
+            role: 'togglefullscreen'
+        },
+
+        {
+            type: 'separator'
+        },
+
+        {
+            label: 'Exit',
+            role: 'quit'
+        },
+    ]
+}]
+const menu = Menu.buildFromTemplate(template)
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -8,6 +35,8 @@ function createWindow() {
             nodeIntegration: true
         }
     })
+
+    Menu.setApplicationMenu(menu)
 
     win.loadFile('index.html')
 
